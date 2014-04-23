f33c0c4560928f7ee2a148ffac1a453137da9a2e
jeffdare <jeffdare@in.ibm.com>
2014-04-23 15:10:39 +0530

    Hostname update

--- iot-1.0/iot.h
+++ iot-1.0/iot.h
@@ -21,7 +21,7 @@
 #define MAXBUF 100
 
 //config properties
-#define MSPROXY_URL "tcp://46.16.188.196:1883"
+#define MSPROXY_URL "tcp://messaging.qs.ibmcloud.com:1883"
 #define EVENTS_INTERVAL 2
 #define TENANT_PREFIX "quickstart"
 #define DEVICE_NAME "myPi"
