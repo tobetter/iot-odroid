71d4eed04c1c26aaa2dae5664625d72e3ebae1e1
Jeffrey Dare <jeffdare@in.ibm.com>
2014-05-26 12:13:34 +0000

    adding new command in iot service for getting deviceID

--- samples/c/iotGetDeviceID.sh
+++ samples/c/iotGetDeviceID.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-/sbin/ifconfig | grep 'eth0' | tr -s ' ' | cut -d ' ' -f5 |  tr -d ':'
+devId=`/sbin/ifconfig | grep 'eth0' | tr -s ' ' | cut -d ' ' -f5 |  tr -d ':'`
+
+echo The device ID is $devId
+echo For Real-time visualization of the data, visit http://quickstart.internetofthings.ibmcloud.com/?deviceId=$devId
+
+
